5902ea482a7a18695b5c15b54dad6ed089dd9ea8
Garima Sharma <gasharma.cse16@chitkarauniversity.edu.in>
2019-10-11 10:17:18 +0530

    cors error solved

--- backend/routers/route.js
+++ backend/routers/route.js
@@ -1,12 +1,26 @@
 const app = require('../config/server')
+const express = require('express')
+const path = require('path')
+const {Router} = require('express')
+const cors = require('cors')
+
+var whitelist = ['http://localhost:5500', 'http://localhost:9000']
+var corsOptions = {
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    }
+  }
 
 module.exports =()=> {
-    const router = express.Router();
-    
-    router.get('/',function(req,res) {
-        let reqPath = path.join(__dirname, '../../Client/views/index.html')
-        res.sendFile(reqPath)
+    const router = Router();
+
+    router.post('/signup',cors(),function(req,res){
+        console.log(req.body)
     })
     
-    return router()
+    return router
 }
