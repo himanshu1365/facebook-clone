bc1436e22d87386f33840372f603b21a2d5872a7
himanshu1365 <himanshu1365.cse16@chitkara.edu.in>
2019-10-15 18:56:36 +0530

    corrected dom

--- Client/public/js/home.js
+++ Client/public/js/home.js
@@ -1,3 +1,99 @@
+
+function showdata(data){
+    
+    for(let i=0;i<data.length;i++){
+        for(let j=0;j<data[i].posts.length;j++){
+            console.log(data[i].posts[j])
+            let body = document.getElementById('show-post-div')
+
+            let postcard = document.createElement("div")
+            postcard.setAttribute('class','card postcard')
+            body.appendChild(postcard)
+
+            let cardheader = document.createElement("div")
+            cardheader.setAttribute('class','card-header')
+            postcard.appendChild(cardheader)
+
+            let dflex = document.createElement("div")
+            dflex.setAttribute('class','d-flex')
+            cardheader.appendChild(dflex)
+            let image = document.createElement('img')
+            image.setAttribute('src','../assets/post.jpg')
+            image.setAttribute('class','rounded-circle postUserPhoto')
+            dflex.appendChild(image)
+            let author = document.createElement("p")
+            author.innerHTML = "Author"
+            dflex.appendChild(author)
+
+            let date = document.createElement('p')
+            date.setAttribute('class','post-date')
+            cardheader.appendChild(date)
+            date.innerHTML = data[i].posts[j].postDate
+
+            let cardbody = document.createElement("div")
+            let postContent = document.createElement("p")
+            postContent.setAttribute("class","text-justify")
+            postContent.innerHTML = data[i].posts[j].postdata
+            cardbody.appendChild(postContent)
+            postcard.appendChild(cardbody)
+
+            let postImage = document.createElement("div")
+            postImage.setAttribute('class','post-img')
+            cardbody.appendChild(postImage)
+            let postImg = document.createElement('img')
+            postImg.setAttribute('src','../assets/post.jpg')
+            postImage.appendChild(postImg)
+
+            let hr = document.createElement("hr")
+            cardbody.appendChild(hr)
+            
+            let likebox = document.createElement("div")
+            likebox.setAttribute("class","like-share-box")
+            let row = document.createElement("div")
+            likebox.appendChild(row)
+            row.setAttribute("class","row")
+            let likecontent = document.createElement("div")
+            likecontent.setAttribute('class','like-share-contents col-md-3')
+            row.appendChild(likecontent)
+            let thumbs = document.createElement('span')
+            let ithumbs = document.createElement('i')
+            ithumbs.setAttribute('class','fa fa-thumbs-o-up')
+            thumbs.appendChild(ithumbs)
+            likecontent.appendChild(thumbs)
+
+            let commentcontent = document.createElement("div")
+            commentcontent.setAttribute('class','like-share-contents col-md-3')
+            row.appendChild(commentcontent)
+            let comment = document.createElement('span')
+            let icomment = document.createElement('i')
+            icomment.setAttribute('class','fa fa-comment-o')
+            comment.appendChild(icomment)
+            commentcontent.appendChild(comment)
+
+            let sharecontent = document.createElement('div')
+            sharecontent.setAttribute('class','col-md-3 like-share-contents')
+            row.appendChild(sharecontent)
+            let share = document.createElement('span')
+            let ishare = document.createElement('i')
+            ishare.setAttribute('class','fa fa-share-square-o')
+            share.appendChild(ishare)
+            sharecontent.appendChild(share)
+
+            let cardfooter = document.createElement("div")
+            cardfooter.setAttribute('class','card-footer')
+            let input = document.createElement('input')
+            input.setAttribute('type','text')
+            input.setAttribute('class','send-comment')
+            input.setAttribute('placeholder','Share your comments')
+            cardfooter.appendChild(input)
+            postcard.appendChild(cardfooter)
+            cardbody.appendChild(likebox)
+        }
+    }
+}
+
+
+
 $(document).ready( function(){
     $.ajax("http://localhost:9000/home/getPosts",{
             type:'GET',
@@ -6,7 +102,7 @@ $(document).ready( function(){
                 token: localStorage.getItem('userToken')
             },
             success: function(data){
-                console.log(data)
+                showdata(data)
             },
             error: function(error){
                 localStorage.removeItem("userToken")
