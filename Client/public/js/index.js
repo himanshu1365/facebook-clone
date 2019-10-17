b3f8a3f9db6576b960ea40418046d40191bf51f0
Garima Sharma <gasharma.cse16@chitkarauniversity.edu.in>
2019-10-17 11:26:23 +0530

    login and signup for with validation

--- Client/public/js/index.js
+++ Client/public/js/index.js
@@ -3,19 +3,20 @@
 $(document).ready( function(){
     document.getElementById('btnSave').addEventListener('click',validateForm)
     function validateForm(){
-        console.log("hrerehere")
-        var fname = document.getElementById("SignUpFirstName")
-        var lname = document.getElementById("SignUpLastName");
-        var email = document.getElementById("SignUpFormEmail");
-        var phone = document.getElementById("SignUpFormPhone");
+        var fname = document.getElementById("SignUpFirstName").value;
+        var lname = document.getElementById("SignUpLastName").value;
+        var email = document.getElementById("SignUpFormEmail").value;
+        var mobile = document.getElementById("SignUpFormPhone").value;
+        var password = document.getElementById("SignUpFormPassword").value;
+        var confirmPassword = document.getElementById("SignUpFormConfirmPassword").value;
         if(fname === "") {
-            alert("Please enter your name");
+            alert("Please enter your first name");
         } 
         else {
             fname = true
             var regex = /^[a-zA-Z\s]+$/;                
             if(regex.test(fname) === false) {
-                alert("Please enter a valid name");
+                alert("Please enter a valid first name");
             } 
             else{
                 fname = true;
@@ -23,13 +24,13 @@ $(document).ready( function(){
         }
 
         if(lname === "") {
-            alert("Please enter your name");
+            alert("Please enter your last name");
         }
         else {
             lname = true
             var regex = /^[a-zA-Z\s]+$/;                
             if(regex.test(lname) === false) {
-                alert("Please enter a valid name");
+                alert("Please enter a valid last name");
             } 
             else{
                 lname = true;
@@ -37,34 +38,42 @@ $(document).ready( function(){
         }
 
         if(email === "") {
-            alert("Please enter your name");
+            alert("Please enter your email");
         }
         else {
             email = true
             var regex = /^[a-zA-Z\s]+$/;                
             if(regex.test(email) === false) {
-                alert("Please enter a valid name");
+                alert("Please enter a valid email");
             } 
             else{
                 email = true;
             }
         }
 
-        if(phone === "") {
-            alert("Please enter your name");
-        }
-        else {
-            phone = true
-            var regex = /^[1-9]\d{9}$/;                
-            if(regex.test(phone) === false) {
-                alert("Please enter a valid name");
-            } 
-            else{
-                phone = true;
+        if(mobile == "") {
+            alert( "Please enter your mobile number");
+        } else {
+          
+            var regex = /^[1-9]\d{9}$/;
+            if(regex.test(mobile) === false) {
+                alert("Please enter a valid 10 digit mobile number");
+            } else{
+                phone= true;
+                
             }
         }
-        
-        if((fname || lname || email || phone )== true) {
+
+        if (password != confirmPassword) {
+            alert("Passwords do not match.");
+            
+        }
+        else{
+            password = true;
+            
+        }
+
+        if((fname || lname || email || phone || password )== true) {
             let signUpData  = {
                 firstName: $('#SignUpFirstName').val(),
                 lastName: $('#SignUpLastName').val(),
@@ -94,4 +103,68 @@ $(document).ready( function(){
     }
 })
 
+
+$(document).ready( function(){
+    document.getElementById('loginbtn').addEventListener('click',validateForm)
+    function validateForm(){
+        
+        var email = document.getElementById("LoginFormEmail").value;
+        
+        var password = document.getElementById("LoginFormPassword").value;
+        
+       
+
+        if(email === "") {
+            alert("Please enter your email");
+        }
+        else {
+            email = true
+            var regex = /^[a-zA-Z\s]+$/;                
+            if(regex.test(email) === false) {
+                alert("Please enter a valid email");
+            } 
+            else{
+                email = true;
+            }
+        }
+
+        
+
+        if (password == "") {
+            alert("Enter Password");
+            
+        }
+        else{
+            password = true;
+            
+        }
+
+        if(( email || password )== true) {
+            let logindata = {
+                email: $('#LoginFormEmail').val(),
+                password: $("#LoginFormPassword").val()
+            }
+            console.log(logindata)
+            $.ajax("http://localhost:9000/login",{
+                type:"POST",
+                dataType:"json",
+                contentType:"application/json",
+                data:JSON.stringify(logindata),
+                success: function(data,status){
+                    console.log("data: "+data)
+                    localStorage.setItem('userToken',data.token)
+                    $(location).attr('href','./views/home.html')
+                },
+                error: function(data,error){
+                    // let statusMessage = JSON.stringify(data)
+                    // console.log('data : '+data.responseJSON)
+                    console.log('error '+ error)
+                }
+            })
+        } 
+        else {
+            return false;
+        }
+    }
+})
 
